8320ed904b9bfd5496cd2f68774c1a5cd3f4289c
SEUNGHOON YEON <yeonsh@gmail.com>
2012-12-17 10:19:18 +0900

    jslint

--- crockford.js
+++ crockford.js
@@ -1,8 +1,9 @@
+"use strict";
+
 function isNumber(arg) {
     return typeof arg === 'number' && isFinite(arg);
 }
 
 function isArray(arg) {
-    return (arg && typeof arg === 'object' && typeof arg.length == 'number' && !(arg.propertyIsEnumerable('length'));
+    return (arg && typeof arg === 'object' && typeof arg.length === 'number' && !(arg.propertyIsEnumerable('length')));
 }
-        
